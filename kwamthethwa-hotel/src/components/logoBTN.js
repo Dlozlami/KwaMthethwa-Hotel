57cf6da7975a822603a9b689d1914c7227a6daf7
Dlozi <dlozi.mthethwa@gmail.com>
2023-08-12 18:35:28 +0200

    assets

--- kwamthethwa-hotel/src/components/logoBTN.js
+++ kwamthethwa-hotel/src/components/logoBTN.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 
 export default function LogoBTN() {
-  const history = useHistory();
   return (
-    <div className="setBTN" onClick={() => history.push("/")}>
-      <img src="../../assets/icons/logoBTN.png" />
+    <div className="setBTN">
+      <img src="../assets/icons/logoBTN.png" alt="official logo" />
     </div>
   );
 }
